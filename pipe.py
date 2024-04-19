b565857c8e2ae1f0b20ec3db6a447b7a2bfef07d
Turnass <antonioprodelgado03@gmail.com>
2024-04-19 15:55:08 +0100

    commit

--- pipe.py
+++ pipe.py
@@ -1,20 +1,65 @@
-import sys
+class PipeManiaState:
+    state_id = 0
 
-def main():
-    # Verifica se um arquivo foi fornecido como argumento na linha de comando
-    if len(sys.argv) != 2:
-        print("Usage: python pipe.py <filename>")
-        return
+    def __init__(self, board):
+        self.board = board
+        self.id = PipeManiaState.state_id
+        PipeManiaState.state_id += 1
 
-    filename = sys.argv[1]  # Obtém o nome do arquivo a partir do argumento de linha de comando
+    def __lt__(self, other):
+        """ Este método é utilizado em caso de empate na gestão da lista
+        de abertos nas procuras informadas. """
+        return self.id < other.id
 
-    # Abre o arquivo fornecido e o lê
-    try:
-        with open(filename, 'r') as file:
-            content = file.read()
-            print("Conteúdo do arquivo:")
-            print(content)
-    except FileNotFoundError:
-        print(f"Arquivo '{filename}' não encontrado.")
+class Board:
+    """ Representação interna de uma grelha de PipeMania. """
 
-main()
+    def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
+        """ Devolve os valores imediatamente acima e abaixo,
+        respectivamente. """
+        # TODO
+        pass
+
+    def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
+        """ Devolve os valores imediatamente à esquerda e à direita,
+        respectivamente. """
+        # TODO
+        pass
+        # TODO: outros metodos da classe
+
+@staticmethod
+def parse_instance():
+    """Lê a instância do problema do standard input (stdin)
+        e retorna uma instância da classe Board.
+        Por exemplo:
+        $ python3 pipe_mania.py < input_T01
+        > from sys import stdin
+        > line = stdin.readline().split()
+    """
+# TODO
+pass
+
+class PipeMania(Problem):
+    def __init__(self, initial_state: Board, goal_state: Board):
+        """ O construtor especifica o estado inicial. """
+        # TODO
+        pass
+
+    def actions(self, state: State):
+        """ Retorna uma lista de ações que podem ser executadas a
+        partir do estado passado como argumento. """
+        # TODO
+        pass
+
+    def result(self, state: State, action):
+        """ Retorna o estado resultante de executar a 'action' sobre
+        'state' passado como argumento. A ação a executar deve ser uma
+        das presentes na lista obtida pela execução de
+        self.actions(state). """
+        # TODO
+        pass
+    
+    def h(self, node: Node):
+        """ Função heuristica utilizada para a procura A*. """
+        # TODO
+        pass
