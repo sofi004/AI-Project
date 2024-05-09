77feb6b7af3d7c35cf65d78b7d5bff89bf6fe2a2
ist1106194 <sophiapiteira@gmail.com>
2024-05-09 20:37:33 +0100

    ee

--- pipe.py
+++ pipe.py
@@ -18,11 +18,12 @@ from search import (
 
 class Board:
 
-    def __init__(self, matrix=None, transformed=None):
+    def __init__(self, matrix=None, transformed=None, actions=None):
         self.matrix = matrix
+        self.transformed = transformed
+        self.actions = actions
         self.rows = len(matrix)
         self.cols = len(matrix[0])
-        self.transformed = transformed
     
     def adjacent_vertical_values(self, row: int, col: int):
         if row-1 < 0 and row+1 > self.rows:
@@ -47,26 +48,41 @@ class Board:
     def get_value(self, row, col):
         return self.matrix[row][col]
     
+    def get_tuple(self, row, col):
+        return self.transformed[row][col]
+    
     def parse_instance():
         matrix = []
         transformed = []
+        actions = []
         dict = {'FC': (0, 1, 0, 0), 'FB': (0, 0, 0, 1), 'FE': (1, 0, 0, 0), 'FD': (0, 0, 1, 0),
                 'BC': (1, 1, 1, 0), 'BB': (1, 0, 1, 1), 'BE': (1, 1, 0, 1), 'BD': (0, 1, 1, 1),
                 'VC': (1, 1, 0, 0), 'VB': (0, 0, 1, 1), 'VE': (1, 0, 0, 1), 'VD': (0, 1, 1, 0),
                 'LH': (1, 0, 1, 0), 'LV': (0, 1, 0, 1)}
+        form_dict = {'F':[[0, 1, 0, 0], [0, 0, 0, 1], [1, 0, 0, 0], [0, 0, 1, 0]],
+                     'B':[[1, 1, 1, 0], [1, 0, 1, 1], [1, 1, 0, 1], [0, 1, 1, 1]],
+                     'V':[[1, 1, 0, 0], [0, 0, 1, 1], [1, 0, 0, 1], [0, 1, 1, 0]],
+                     'L':[[1, 0, 1, 0], [0, 1, 0, 1]]
+                    }
+
         rows = stdin.read().strip().split('\n')
         for row in rows:
             matrix.append(row.split())
         for x in range(len(matrix)):
             transformed_row = []
+            actions_row = []
             for y in range(len(matrix[0])):
                 if matrix[x][y] in dict:
                     transformed_row.append(dict.get(matrix[x][y]))
                 else:
-                    # If column not found in dictionary, assign a default value
                     transformed_row.append(matrix[x][y])
+                if matrix[x][y][0] in form_dict:
+                    actions_row.append(form_dict.get(matrix[x][y][0]))
+                else:
+                    transformed_row.append(matrix[x][y][0])
             transformed.append(transformed_row)
-        return Board(matrix, transformed)
+            actions.append(actions_row)
+        return Board(matrix, transformed, actions)
 
 class PipeManiaState:
     state_id = 0
@@ -104,26 +120,26 @@ class PipeMania(Problem):
         # Retorna uma lista de ações que podem ser executadas a
         # partir do estado passado como argumento.
         # TODO
-        actions = []
         for row in range(state.board.rows):
             for col in range(state.board.cols):
-                positions = "ECDB"
-                block = state.board.get_value(row, col)
-                positions = positions.replace(block[1], "")
-                vertical_values = state.board.adjacent_vertical_values(row, col)
-                horizontal_values = state.board.adjacent_horizontal_values(row, col)
-                if vertical_values[0] == "None":
-                    positions = positions.replace("C", "")
-                if vertical_values[1] == "None":
-                    positions = positions.replace("B", "")
-                if horizontal_values[0] == "None":
-                    positions = positions.replace("E", "")
-                if horizontal_values[1] == "None":
-                    positions = positions.replace("D", "")
-                for character in positions:
-                    actions.append((row, col, character))
-        print(actions)
-        return actions
+                block = state.board.get_tuple(row, col)
+                if state.board.horizontal_values[0] == "None":
+                    for sublista in state.board.actions[row][col]:
+                        if sublista[0] == 1:
+                            state.board.actions[row][col].remove(sublista)
+                if state.board.vertical_values[0] == "None":
+                    for sublista in state.board.actions[row][col]:
+                        if sublista[1] == 1:
+                            state.board.actions[row][col].remove(sublista)
+                if state.board.horizontal_values[1] == "None":
+                    for sublista in state.board.actions[row][col]:
+                        if sublista[2] == 1:
+                            state.board.actions[row][col].remove(sublista)
+                if state.board.vertical_values[1] == "None":
+                    for sublista in state.board.actions[row][col]:
+                        if sublista[3] == 1:
+                            state.board.actions[row][col].remove(sublista)
+        return state.board.actions
 
     def result(self, state: PipeManiaState, action: tuple):
         # Retorna o estado resultante de executar a 'action' sobre
@@ -131,6 +147,10 @@ class PipeMania(Problem):
         # das presentes na lista obtida pela execução de
         # self.actions(state).
         # TODO
+        tuple_to_value = {(0, 1, 0, 0): 'FC', (0, 0, 0, 1): 'FB', (1, 0, 0, 0): 'FE', (0, 0, 1, 0): 'FD',
+                          (1, 1, 1, 0): 'BC', (1, 0, 1, 1): 'BB', (1, 1, 0, 1): 'BE', (0, 1, 1, 1): 'BD',
+                          (1, 1, 0, 0): 'VC', (0, 0, 1, 1): 'VB', (1, 0, 0, 1): 'VE', (0, 1, 1, 0): 'VD',
+                          (1, 0, 1, 0): 'LH', (0, 1, 0, 1): 'LV'}
         if (action in self.actions(state)):
             new_state = PipeManiaState(state.board)
             new_state.board.matrix[action[0]][action[1]] = new_state.board.matrix[action[0]][action[1]][0] + action[2]
@@ -153,6 +173,10 @@ def main():
     print(board.transformed[0][0], board.transformed[0][1], board.transformed[0][2])
     print(board.transformed[1][0], board.transformed[1][1], board.transformed[1][2])
     print(board.transformed[2][0], board.transformed[2][1], board.transformed[2][2])
+    print("\nactions_matrix:\n")
+    print(board.actions[0][0], board.actions[0][1], board.actions[0][2])
+    print(board.actions[1][0], board.actions[1][1], board.actions[1][2])
+    print(board.actions[2][0], board.actions[2][1], board.actions[2][2])
     print("\nadjacent values:")
     print(board.adjacent_vertical_values(0, 0))
     print(board.adjacent_horizontal_values(0, 0))
